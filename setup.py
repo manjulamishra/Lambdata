39edf084c5da5cdd3d0d1c7265223cfdfd682b80
manjula mishra <manjula.mishra@gmail.com>
2019-02-12 11:01:42 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+package setup/insert/installation and metadata for lambdata
+"""
+
+import setuptools
+REQUIRED = [
+        "numpy",
+        "pandas"
+]
+with open ("README.md", "r") as fh:
+    LONG_DESCRIPTION =fh.read()
+
+setuptools.setup(
+        name="Lambdata-manjulamishra",
+        version="0.0.1",
+        author="manjulamishra",
+        description="A collection of Data Science helper funtions",
+        long_description=LONG_DESCRIPTION,
+        long_description_contect_type="text/markdown",
+        url="https://github.com/manjulamishra/Lambdata",
+        packages=setuptools.find_packages(),
+        python_requires=">=3.5",
+        install_requires=REQUIRED,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+            ]
+)
